4f58e087ebba91269089bb98c14e682d5b9dda7a
anasoualkadi <oualkadi.anas@gmail.com>
2023-11-05 18:31:29 +0100

    init docker and docker compose

--- customer-service/src/main/java/com/example/customerservice/controllers/CustomerController.java
+++ customer-service/src/main/java/com/example/customerservice/controllers/CustomerController.java
@@ -3,11 +3,9 @@ package com.example.customerservice.controllers;
 import com.example.customerservice.models.Customer;
 import com.example.customerservice.repostitories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,11 @@ public class CustomerController {
                 .orElseThrow(null);
 
         return ResponseEntity.ok(customer);
+    }
+
+    @PostMapping("/createCustomers")
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer = customerRepository.save(customer);
+        return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
     }
 }
